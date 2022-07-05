544be16c39317268fa24c37ae49b5e42178ed7a2
nazaninsnr <103143082+nazaninsnr@users.noreply.github.com>
2022-07-05 15:57:29 +0430

    Style the application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import Dictionary from "./Dictionary";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="divine" />
         </main>
+        <footer className="App-footer">
+          <small>Coded by Nazanin Naghavi</small>
+        </footer>
       </div>
     </div>
   );
