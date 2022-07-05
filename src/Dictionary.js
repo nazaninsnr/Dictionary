87638890833e5a407754aff333a44adf1c69b562
nazaninsnr <103143082+nazaninsnr@users.noreply.github.com>
2022-07-05 12:37:28 +0430

    Added API Documentation URL

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -10,7 +10,7 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-
+    //Documentation: https://api.dictionaryapi.dev/
     let apiUrl = ` https://api.dictionaryapi.dev/api/v2/entries/en_US/ ${keyword}/`;
     axios.get(apiUrl).then(handleResponse);
   }
